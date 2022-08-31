42789f2159ffea02310086fca59a05b14450a694
theScarlettCoder <egbadonjeffrey@gmail.com>
2022-08-31 21:28:25 +0100

    First Boiler Plate Code How to filter with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Users } from "./user";
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  console.log(
+    Users.filter((user) => user.first_name.toLowerCase().includes("fe"))
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Filter App</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
+      <ul className="list">
+        {Users.filter((user) =>
+          user.first_name.toLowerCase().includes(query)
+        ).map((user) => (
+          <li key={user.id} className="listItem">
+            {user.first_name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
